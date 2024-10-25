ffdb65561d73e7a11c9740958490c677fffacb74
Léster Davletbaev <70449559+mutharphuker@users.noreply.github.com>
2024-10-25 17:43:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def send_message(message):
 	try:
 		statuss = bot.send_message(message.chat.id, lng[f'{message.from_user.language_code}'][2])
 		ydl_opts = {
-			'format': bestvideo[height<=1080]+bestaudio/best[height<=1080],
+			'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
 			'outtmpl': '%(title)s.%(ext)s',
 		}
 		with yt_dlp.YoutubeDL(ydl_opts) as ydl:
